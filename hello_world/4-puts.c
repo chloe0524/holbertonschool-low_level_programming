6f60d9a9b9003a3294dc6e5e3648a96dd756678d
chloe0524 <chloco31@gmail.com>
2023-10-10 09:18:30 -0700

    c script 1

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,12 +2,17 @@
  * file name: 4-puts.c
  * made by Chloe.c
  */
+
 #include <stdio.h>
+
 /**
- * main function print "Programming is like building a multilingual
- *                puzzle, followed by a new line.
- * Return:0
+ * main - Entry point
+ *
+ * Description: 'print sentence'
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
