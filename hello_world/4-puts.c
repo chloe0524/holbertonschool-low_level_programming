2767765de2cfac8272ed033b3fc5870882cb4c0d
chloe0524 <chloco31@gmail.com>
2023-10-10 06:41:19 -0700

    print string of text

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,13 @@
+/*
+ * file name: 4-puts.c
+ * made by Chloe.c 
+ */
 #include <stdio.h>
+/**
+ * main function print "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ * Return:0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
