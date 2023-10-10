c700ed898775aae31c38cc75ac79cc4fe19ddc32
chloe0524 <chloco31@gmail.com>
2023-10-10 06:34:05 -0700

    print string of text

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-int main(voiid)
+int main(void)
 {
-	fputs("\"Programming is like building a multilingual puzzle\n");
-return (0);
-}
+	puts("\"Programming is like building a multilingual puzzle");
 
+	return (0);
+}
