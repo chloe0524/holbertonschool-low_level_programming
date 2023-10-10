66042752c27a806bc12e630986a8165becff4287
chloe0524 <chloco31@gmail.com>
2023-10-10 09:26:20 -0700

    first printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,21 @@
+/*
+ * file name: 5-printf.c
+ * made by Chloe.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'printf'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
