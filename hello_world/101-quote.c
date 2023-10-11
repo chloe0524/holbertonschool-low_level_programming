84fdb398b851cd8374ec1aef9f0da4c7a31dbb49
chloe0524 <chloco31@gmail.com>
2023-10-11 04:50:40 -0700

    printf return erorr

---
+++ hello_world/101-quote.c
@@ -0,0 +1,21 @@
+/*
+ * file name: 4-puts.c
+ * made by Chloe.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print sentence'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        puts("and that piece of art is useful"\- Dora Korpar, 2015-10-19");
+
+        return (1);
+}
