dca3058df182e278610cb57174e0714b9f9704fd
chloe0524 <chloco31@gmail.com>
2023-10-11 00:37:42 -0700

    sizeof in c + printf

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,11 +14,11 @@
  */
 int main(void)
 {
-	printf("Size char: %zu byte(s)\n", sizeof(char));
-	printf("Size int: %zu byte(s)\n", sizeof(int));
-	printf("Size long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
 }
