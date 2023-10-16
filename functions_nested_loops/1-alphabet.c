afddffe2115043bf6057a373a958fa335964793d
chloe0524 <chloco31@gmail.com>
2023-10-16 10:35:40 -0700

    nested loop 10 alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,12 +8,13 @@
 
 void print_alphabet(void)
 {
-	char alphabet = 'a';
+	char alphabet;
 
-	while (alphabet <= 'z')
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		_putchar(alphabet);
-		alphabet++;
+		
 	}
 	_putchar('\n');
 }
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ * Return: nothing
+ */
+
+void print_alphabet_x10(void)
+{
+        char alphabet;
+	int i;
+
+	for (i=1; i <= 10; i++) 
+	{
+        	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+        	{
+                	_putchar(alphabet);
+
+        	}
+        	_putchar('\n');
+	}
+}
