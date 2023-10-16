479a671647a9706aa38e40b804f3ecfea78e5b00
chloe0524 <chloco31@gmail.com>
2023-10-16 05:39:22 -0700

    print combinaison single digits with ,

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+{
+putchar(num + '0');
+if (num < 9)
+{
+putchar(',');
+}
+}
+
+putchar('\n');
+
+return (0);
+}
+
