a2ef0c65050e3244e77c8cf3be229ea4dd648af9
chloe0524 <chloco31@gmail.com>
2023-10-16 04:48:18 -0700

    alphabet reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 97; i <= 122; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
