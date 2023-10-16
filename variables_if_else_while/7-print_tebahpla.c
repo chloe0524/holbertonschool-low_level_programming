d7e0e869e81cde46679ecd95879bce0205fc0c11
chloe0524 <chloco31@gmail.com>
2023-10-16 04:57:33 -0700

    reverse alphabet

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -6,12 +6,9 @@
  */
 int main(void)
 {
-int i;
-
-for (i = 97; i <= 122; i--)
-{
-putchar(i);
-}
+char letter;
+for (letter = 'z'; letter >= 'a'; letter--)
+putchar(letter);
 putchar('\n');
 return (0);
 }
