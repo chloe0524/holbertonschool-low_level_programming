0ef133e251660859c349fe8b79357b3ee5b5bded
chloe0524 <chloco31@gmail.com>
2023-10-16 09:27:04 -0700

    print _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char chainput[]= "_putchar";
+	
+	for (i = 0; i <= 8; i++)
+	{
+		putchar(chainput[i]);
+	}
+
+	putchar('\n');
+	return (0);
+}
