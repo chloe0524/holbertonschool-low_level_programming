bf8884ce48709ee8d994d31aaee53bd419cc7a80
chloe0524 <chloco31@gmail.com>
2023-10-16 01:33:55 -0700

    random n positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -7,22 +7,39 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-    if (n > 0) {
-        printf("%d is positive\n", n);
-    } else if (n == 0) {
-        printf("%d is zero\n", n);
-    } else {
-        printf("%d is negative\n", n);
-    }
+	if (n > 0) {
+		printf("%d is positive\n", n);
+	} else if (n == 0) {
+		printf("%d is zero\n", n);
+	} else {
+		printf("%d is negative\n", n);
+	}
 
-    return (0);
+	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
