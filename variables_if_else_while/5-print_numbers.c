74a81567a8164c32246acadb820be4adcdd31eca
chloe0524 <chloco31@gmail.com>
2023-10-16 05:14:15 -0700

    all single digits base 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int number;
+
+for (number = 0; number < 10; number++)
+{
+printf("%d", number);
+}
+
+putchar('\n');
+return (0);
+}
