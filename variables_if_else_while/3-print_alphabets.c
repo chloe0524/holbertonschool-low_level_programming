f974ace2fa848f402ebca71a02322ef6f6c115c1
chloe0524 <chloco31@gmail.com>
2023-10-16 04:25:36 -0700

    lowercase and upercase alphabet

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -6,18 +6,17 @@
  */
 int main(void)
 {
-    int i;
+int i;
 
-    for (i = 97; i <= 122; i++)
-    {
-        putchar(i);
-    }
+for (i = 97; i <= 122; i++)
+{
+putchar(i);
+}
 
-    for (i = 65; i <= 90; i++)
-    {
-        putchar(i);
-    }
-
-    putchar('\n');
-    return (0);
+for (i = 65; i <= 90; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
 }
