a03c093e85d0187824ca11298e0e54bfca042ca2
chloe0524 <chloco31@gmail.com>
2023-10-17 02:29:43 -0700

    adds two integers and returns the result.

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @number1: first integer to be added.
+ * @number2: second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int add(int number1, int number2)
+{
+	return (number1 + number2);
+}
---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ * @n: integer with last digit printed
+ * Return: Always 0
+ */
+void jack_bauer(void);
+{ 
+
