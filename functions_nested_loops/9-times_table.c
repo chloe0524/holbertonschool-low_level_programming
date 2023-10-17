dfba8367bb055758c97a796dd45d2a17d72071e5
chloe0524 <chloco31@gmail.com>
2023-10-17 11:52:42 -0700

    table of 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * times_table -  prints 9 times table, starting with 0
+ * @i , @j to multiply
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		printf("%d, ", i * 0);
+		for (j = 1 ; j <= 8 ; j++)
+		{
+			printf("%2d, ", i * j);
+		}
+		printf("%2d$\n", i * j);
+	}
+}
