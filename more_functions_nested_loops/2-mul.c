d58b76cea45f319588cb8387c3d082770a80ca05
chloe0524 <chloco31@gmail.com>
2023-10-18 03:50:00 -0700

    multiplies two integers.

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Adds two integers.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: The result of the addition.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
 int _isupper(int c);
 
 int _isdigit(int c);
+
+int mul(int a, int b);
