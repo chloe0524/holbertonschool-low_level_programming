2f0ae315ca4c9d6d17a5e5c92974e8b63a5a43c4
chloe0524 <chloco31@gmail.com>
2023-10-18 04:39:21 -0700

    function prints 10 times numbers 0 to 14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - print number 0 to 14 ten times
+ * Return: nothing
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,3 +7,5 @@ int _isdigit(int c);
 int mul(int a, int b);
 
 void print_numbers(void);
+
+void more_numbers(void);
