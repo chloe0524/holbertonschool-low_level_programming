6f8f74e8087db3fc5ff68dbef20e06cfaea06ab1
chloe0524 <chloco31@gmail.com>
2023-10-18 06:00:31 -0700

    fizz buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 
 /**
- * main -
+ * main - Entry point
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
 
-	for(i=1; i<=100; ++i)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
+		{
 			printf("Fizz");
-		if (i % 5 == 0)
+			if (i % 5 == 0)
+				printf(" ");
+		}
+		else if (i % 5 == 0)
+		{
 			printf("Buzz");
-		if ((i % 3 != 0) && (i % 5 != 0))
-			printf("number=%d", i);
-		printf("\n");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
 	}
+	printf("\n");
 	return (0);
 }
