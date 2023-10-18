ea93dd9458e74e9e48ea0b9e9a6604bc177dd694
chloe0524 <chloco31@gmail.com>
2023-10-18 06:10:23 -0700

    fizz buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,24 +11,15 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
-		{
 			printf("Fizz");
-			if (i % 5 == 0)
-				printf(" ");
-		}
-		else if (i % 5 == 0)
-		{
+		if (i % 5 == 0)
 			printf("Buzz");
-		}
-		else
-		{
+		if (i % 3 != 0 && i % 5 != 0)
 			printf("%d", i);
-		}
-		if (i != 100)
-		{
+		if (i < 100)
 			printf(" ");
-		}
 	}
 	printf("\n");
+
 	return (0);
 }
