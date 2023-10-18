2860630bb7c4c990b4f0e5be88c07c1dff8cf9f9
chloe0524 <chloco31@gmail.com>
2023-10-18 04:59:40 -0700

    straight lines

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -7,15 +7,12 @@
  */
 void print_line(int n)
 {
-	int a = 0;
+	if (n <= 0)
+	n = 1;
 
-	if (n > 0)
+	while (n-- > 0)
 	{
-		while (a<= n)
-		{
-			_putchar('_');
-			a++;
-		}
-	_putchar('\n');
+	_putchar('_');
 	}
+    _putchar('\n');
 }
