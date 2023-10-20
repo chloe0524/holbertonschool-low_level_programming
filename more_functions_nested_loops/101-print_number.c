776dc3d05cbf5fcdafdc4be05b5e63430a641177
chloe0524 <chloco31@gmail.com>
2023-10-20 05:01:56 -0700

    print integer

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - print integer
+ * @n:  integer to print
+ * description: only use _putchar
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -19,3 +19,5 @@ void print_diagonal(int n);
 void print_square(int size);
 
 void print_triangle(int size);
+
+void print_number(int n);
