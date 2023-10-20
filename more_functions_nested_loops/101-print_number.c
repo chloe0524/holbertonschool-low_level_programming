26a319f886eda4f686628e1cd2f05657e63f9953
chloe0524 <chloco31@gmail.com>
2023-10-20 05:05:10 -0700

    print integer

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -14,9 +14,14 @@ void print_number(int n)
 		n = -n;
 	}
 
-	if (n / 10 != 0)
+	if (n == 0)
 	{
-		print_number(n / 10);
+		_putchar('0');
+		return;
 	}
+
+	if (n / 10)
+		print_number(n / 10);
+
 	_putchar((n % 10) + '0');
 }
