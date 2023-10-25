c0c12d8d0c6224fa72bc628775fb3a5615f80a1c
chloe0524 <chloco31@gmail.com>
2023-10-25 02:03:14 -0700

    print string

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _puts: print the string
+ * _puts - print the string
  * @str: char to print string
  * Return: nothing
  */
 
 void _puts(char *str)
 {
-        for (; *str != '\0'; str++)
-        {
-                _putchar(*str);
-        }
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
 
-        _putchar('\n');
+	_putchar('\n');
 }
