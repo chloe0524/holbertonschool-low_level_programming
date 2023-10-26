8bb32135da6bd9e9e0d08971ce0d1a6dd3dbff2f
chloe0524 <chloco31@gmail.com>
2023-10-26 05:15:32 -0700

    convert a string to an integer

--- pointers_arrays_strings/100-main.c
+++ pointers_arrays_strings/100-main.c
@@ -1,30 +1,17 @@
-#include "main.h"
 #include <stdio.h>
 
+int _atoi(char *);
+
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int nb;
+	int n;
 
-    nb = _atoi("98");
-    printf("%d\n", nb);
-    nb = _atoi("-402");
-    printf("%d\n", nb);
-    nb = _atoi("          ------++++++-----+++++--98");
-    printf("%d\n", nb);
-    nb = _atoi("214748364");
-    printf("%d\n", nb);
-    nb = _atoi("0");
-    printf("%d\n", nb);
-    nb = _atoi("Suite 402");
-    printf("%d\n", nb);
-    nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-    printf("%d\n", nb);
-    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-    printf("%d\n", nb);
-    return (0);
+	n = _atoi("4");
+	printf("%d\n", n);
+	return (0);
 }
