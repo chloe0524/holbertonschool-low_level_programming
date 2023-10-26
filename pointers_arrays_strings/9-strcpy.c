24a3c4ca4185c5ddd0a4e5460c9463650951997b
chloe0524 <chloco31@gmail.com>
2023-10-26 00:59:01 -0700

    copy string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,18 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * _strcpy - copy string
- *@dest: string
- *@src: string
+ * _strcpy - Copy paste string
+ *@dest: destination
+ *@src: source
  *Return: dest
  */
-
-char *_strcpy(char *dest,  char *src)
+char *_strcpy(char *dest, char *src)
 {
-	int i;
-
-	while ((*src + i) != '\0')
+	int inc = 0;
+	while (*(src + inc) != '\0')
 	{
-		*(dest + i) = *(src + i);
-		i++;
+		*(dest + inc) = *(src + inc);
+		inc++;
 	}
+	*(dest + inc) = '\0';
 	return (dest);
-
 }
