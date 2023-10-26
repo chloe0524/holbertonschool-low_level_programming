bcc3324780e1dc886948bead540c4e2b5744a0bc
chloe0524 <chloco31@gmail.com>
2023-10-26 08:49:22 -0700

    convert a string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -32,31 +32,3 @@ int _atoi(char *nombre)
 	entier = entier * negatif;
 	return (entier);
 }
-
-/**
- * main - Entry point of program.
- * Return: 0
- */
-
-int main(void)
-{
-	int nb;
-
-	nb = _atoi("98");
-	printf("%d\n", nb);
-	nb = _atoi("-402");
-	printf("%d\n", nb);
-	nb = _atoi("	------++++++-----+++++--98");
-	printf("%d\n", nb);
-	nb = _atoi("214748364");
-	printf("%d\n", nb);
-	nb = _atoi("0");
-	printf("%d\n", nb);
-	nb = _atoi("Suite 402");
-	printf("%d\n", nb);
-	nb = _atoi("	+     +   -   -98 Battery Street; San Francisco, CA 94111-USA	");
-	printf("%d\n", nb);
-	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-	printf("%d\n", nb);
-	return (0);
-}
