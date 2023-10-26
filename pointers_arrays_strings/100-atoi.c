da66b1cd58df043975b50748a2604ac6e55bd0a3
chloe0524 <chloco31@gmail.com>
2023-10-26 04:56:18 -0700

    convert a string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,19 +1,44 @@
-#include "main.h"
-/**
- * _strcpy - Copy paste string
- *@dest: destination
- *@src: source
- *Return: dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
+#include <stdio.h>
 
-	while (*(src + i) != '\0')
-	{
-		*(dest + i) = *(src + i);
-		i++;
+int _atoi(char* nombre) {
+	int i;
+	int negatif = 1;
+	int entier = 0;
+	int traitementfait = 0;
+
+	for (i = 0; nombre[i] != 0; i++) {
+		if (nombre[i] == '-')
+			negatif = negatif * -1;
+
+		if (nombre[i] >= '0' && nombre[i] <= '9') {
+			entier = (entier * 10) + nombre[i] - '0';
+			traitementfait = 1;
+		} else if (traitementfait == 1)
+			break;
 	}
-	*(dest + i) = '\0';
-	return (dest);
+
+	entier = entier * negatif;
+	return entier;
+}
+
+int main() {
+	int nb;
+
+	nb = _atoi("98");
+	printf("%d\n", nb);
+	nb = _atoi("-402");
+	printf("%d\n", nb);
+	nb = _atoi("          ------++++++-----+++++--98");
+	printf("%d\n", nb);
+	nb = _atoi("214748364");
+	printf("%d\n", nb);
+	nb = _atoi("0");
+	printf("%d\n", nb);
+	nb = _atoi("Suite 402");
+	printf("%d\n", nb);
+	nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
+	printf("%d\n", nb);
+	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+	printf("%d\n", nb);
+	return 0;
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,6 +20,8 @@ void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
 
+int _atoi(char *s);
+
 char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
