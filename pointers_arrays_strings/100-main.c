f29b5518e008fb90aca371f22743db702c0de61b
chloe0524 <chloco31@gmail.com>
2023-10-26 05:22:07 -0700

    copy string

--- pointers_arrays_strings/100-main.c
+++ pointers_arrays_strings/100-main.c
@@ -1,17 +1,30 @@
+#include "main.h"
 #include <stdio.h>
 
-int _atoi(char *);
-
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int n;
+    int nb;
 
-	n = _atoi("4");
-	printf("%d\n", n);
-	return (0);
+    nb = _atoi("98");
+    printf("%d\n", nb);
+    nb = _atoi("-402");
+    printf("%d\n", nb);
+    nb = _atoi("          ------++++++-----+++++--98");
+    printf("%d\n", nb);
+    nb = _atoi("214748364");
+    printf("%d\n", nb);
+    nb = _atoi("0");
+    printf("%d\n", nb);
+    nb = _atoi("Suite 402");
+    printf("%d\n", nb);
+    nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
+    printf("%d\n", nb);
+    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+    printf("%d\n", nb);
+    return (0);
 }
--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,14 +12,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
-	
-	for( i = 0 ; src[i] !=0 && i < n; i++)
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
+	for (; n > i; i++)
+	{
+		dest[i] = '\0';
+	}
 
-	dest[i] = '\0';
 	return (dest);
 }
-
