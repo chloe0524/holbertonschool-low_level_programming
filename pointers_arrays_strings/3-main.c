d05c3da39748e400dda6104467f4d52393d9a308
chloe0524 <chloco31@gmail.com>
2023-10-27 00:38:24 -0700

    compare two strings

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - check the code
@@ -7,9 +8,11 @@
  */
 int main(void)
 {
-	char *str;
+    char s1[] = "Hello";
+    char s2[] = "World!";
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	_puts(str);
-	return (0);
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
+    return (0);
 }
