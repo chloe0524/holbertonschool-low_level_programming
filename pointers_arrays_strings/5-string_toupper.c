bd520c4472f1c0ba84baa576ec8540c12a7978fd
chloe0524 <chloco31@gmail.com>
2023-10-27 01:34:45 -0700

    lowercase to uppercase

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -8,14 +8,17 @@
  * Return: Pointer modified string
  */
 
-char string_toupper(char *a)
-{
 
+char *string_toupper(char *a)
+{
 	int i;
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		a[i] = toupper(a[i]); 
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			a[i] = a[i] - 32;
+		}
 	}
 
 	return a;
