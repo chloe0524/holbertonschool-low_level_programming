979c6abfbc769eef1e168655e5c5506cf8cb77bc
chloe0524 <chloco31@gmail.com>
2023-11-07 05:00:27 -0800

    more malloc and exit

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
 
-/*
- *
- *
+/**
+ * *malloc_checked - allocate memory with malloc
+ * @b: number of bytes
+ * Return: pointer to memory
  */
 
 void *malloc_checked(unsigned int b)
 {
 	int *p;
 
-	p = malloc(b);
+	p = malloc(sizeof(b));
 
 		if (p == NULL)
-		{	
-			exit(98);	
+		{
+			exit(98);
 		}
 	return (p);
 }
