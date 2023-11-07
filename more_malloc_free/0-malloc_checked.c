8cd1d7bb811df9f8184628b8f9dc3af033cf7f47
chloe0524 <chloco31@gmail.com>
2023-11-07 04:51:52 -0800

    more malloc and exit

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -10,12 +10,12 @@ void *malloc_checked(unsigned int b)
 {
 	int *p;
 
-	p = malloc(sizeof(unsigned int) * b);
+	p = malloc(b);
 
-	if (p == NULL)
-	{	
-		exit(98);	
-	}
+		if (p == NULL)
+		{	
+			exit(98);	
+		}
 	return (p);
 }
 
