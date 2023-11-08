54c1a47fcd14f5b775cb9502b6af5eca24e215e8
chloe0524 <chloco31@gmail.com>
2023-11-08 13:57:29 -0800

    computes the absolute value of a number x

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,5 +1,6 @@
-#ifndef TASK_3_FUNCTION_LIKE_MACRO_H
-#define TASK_3_FUNCTION_LIKE_MACRO_H 
+#ifndef FUNCTION_MACRO_H
+#define FUNCTION_MACRO_H
 
-#define ABS(x) (x >= 0 ? (x) : (-x))
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
 #endif
