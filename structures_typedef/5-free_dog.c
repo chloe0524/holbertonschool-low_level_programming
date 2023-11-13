afbe0570c3085e7264d506ab873a23c9212cc992
chloe0524 <chloco31@gmail.com>
2023-11-13 05:20:49 -0800

    free the dogs

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free the dogs
+ * @d number of dogs
+ *
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+	int i;
+	int num = 0;
+
+	for (i = 0; i < num; i++)
+	{
+		free(d[i].name);
+		free(d[i].owner);
+	}
+
+	free(d);
+}
+
