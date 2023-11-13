e48cd53ff56552b0e1003643bb7ae370e4af8a5b
chloe0524 <chloco31@gmail.com>
2023-11-13 01:33:18 -0800

    struct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct_dog - intializes a variable of type struct dog
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * description:
+ * defines a structure 'struct_dog' to store information about a dog,
+ * including its name, age, and owner.
+ * Return: nothing
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG */
