ebb6d6bf1aff3fffbb80bc4ca221be72c0949968
chloe0524 <chloco31@gmail.com>
2023-11-13 02:52:58 -0800

    struct

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -21,14 +21,8 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 	}
 
-	if (d->age == 0.0)
-	{
-		printf("Age: (nil)\n");
-	}
-	else
-	{
-		printf("Age: %.6f\n", d->age);
-	}
+		printf("Age: %f\n", d->age);
+	
 
 	if (d->owner == NULL)
 	{
@@ -39,4 +33,5 @@ void print_dog(struct dog *d)
 		printf("Owner: %s\n", d->owner);
 	}
 }
+
 
