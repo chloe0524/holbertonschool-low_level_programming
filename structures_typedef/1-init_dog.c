dbc1e3bbe58a85f09c42b375bdd2e024d8abaf07
chloe0524 <chloco31@gmail.com>
2023-11-13 02:09:30 -0800

    struct

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -11,8 +11,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d == NULL)
+		return;
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
