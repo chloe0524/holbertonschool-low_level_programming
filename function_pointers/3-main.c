fc2e979146cef1d0512fad8953562663882a9a4b
chloe0524 <chloco31@gmail.com>
2023-11-15 01:01:30 -0800

    main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -10,6 +10,7 @@
  *
  * Return: 0 on success.
  */
+
 int main(int argc, char *argv[])
 {
 	int a, b;
@@ -21,21 +22,23 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (98);
 	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	f = get_op_func(argv[2]);
 
-	if (f == NULL)
+	if (f == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		return (99);
 	}
+
 	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
 	{
 		printf("Error\n");
 		return (100);
-
 	}
+
 	rslt = f(a, b);
 
 	printf("%d\n", rslt);
