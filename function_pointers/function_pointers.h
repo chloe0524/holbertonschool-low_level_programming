8ade61654ee51458a34bd3887ed5b7752894beca
chloe0524 <chloco31@gmail.com>
2023-11-15 00:51:47 -0800

    function pointers

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,8 +1,10 @@
-int _putchar(char c);
+#ifndef FUNC_POINTERS_H
+#define FUNC_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
-
 void array_iterator(int *array, size_t size, void (*action)(int));
-
 int int_index(int *array, int size, int (*cmp)(int));
 
+int _putchar(char c);
+
+#endif
