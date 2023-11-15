64093ca78b98cb386a55de5f12bd9b830e728e04
chloe0524 <chloco31@gmail.com>
2023-11-15 02:07:22 -0800

    header file

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifdef VAR_H
+#define VAR_H
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
