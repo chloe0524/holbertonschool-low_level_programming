3761516cb0615bdf7d5800849df79b2618c5fd21
chloe0524 <chloco31@gmail.com>
2023-11-15 11:39:55 -0800

    header file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,16 +1,22 @@
 #ifndef VAR_FUNCTIONS_H
 #define VAR_FUNCTIONS_H
+
 #include <stdarg.h>
+
+/**
+ * struct tdisplay - Format specifier and display function pair
+ * @format_type: Format specifier character ('c', 'i', 'f', 's')
+ * @fdisplay: Pointer to the display function
+ */
+typedef struct tdisplay
+{
+	char format_type;
+	void (*fdisplay)(va_list);
+} tdisplay;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-typedef struct
-{
-    char format_type;
-    void (*fdisplay)(va_list);
-} tdisplay;
-
-#endif
+#endif /* VAR_FUNCTIONS_H */
