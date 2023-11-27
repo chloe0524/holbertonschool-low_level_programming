d213a4e4403b7a2e4a053fa1af6b80229dd0f432
chloe0524 <chloco31@gmail.com>
2023-11-27 06:19:50 -0800

    fixed issue with pointer segmentation core dump

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -9,37 +9,38 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *other_node = malloc(sizeof(list_t));
+	list_t *other_node;
 	list_t *end_node;
 
-	while (other_node != NULL || head != NULL || str != NULL)
+	while (!(other_node = malloc(sizeof(list_t))) || !head || !str)
 	{
-		other_node->str = strdup(str);
-		if (!other_node->str)
-		{
-			free(other_node);
-			return (NULL);
-		}
-
-		other_node->len = strlen(str);
-		other_node->next = NULL;
-
-		if (!*head)
-		{
-			*head = other_node;
-		}
-		else
-		{
-			end_node = *head;
-			while (end_node->next)
-			{
-				end_node = end_node->next;
-			}
-			end_node->next = other_node;
-		}
-
-		return (other_node);
+		free(other_node);
+		return NULL;
 	}
-	return (NULL);
-}
 
+	other_node->str = strdup(str);
+	if (!other_node->str)
+	{
+		free(other_node);
+		return NULL;
+	}
+
+	other_node->len = strlen(str);
+	other_node->next = NULL;
+
+	if (!*head)
+	{
+		*head = other_node;
+	}
+	else
+	{
+		end_node = *head;
+		while (end_node->next)
+		{
+			end_node = end_node->next;
+		}
+		end_node->next = other_node;
+	}
+
+	return other_node;
+}
