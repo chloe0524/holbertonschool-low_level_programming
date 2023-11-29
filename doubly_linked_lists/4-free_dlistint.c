8132c8b25399e281140b4ac21e69ed60a8bc6cc7
chloe0524 <chloco31@gmail.com>
2023-11-29 00:59:47 -0800

    free memory alocated

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -4,13 +4,21 @@
  * free_dlistint - frees memory occupied by list
  * @head: pointer to head of linked list.
  */
-
 void free_dlistint(dlistint_t *head)
 {
 	if (head)
 	{
-		free_dlistint(head->next);
-		free_dlistint(head->prev);
+		dlistint_t *next_node = head->next;
+		dlistint_t *prev_node = head->prev;
+
+		if (prev_node)
+			prev_node->next = next_node;
+		if (next_node)
+			next_node->prev = prev_node;
+
+		free_dlistint(next_node);
+		free_dlistint(prev_node);
 		free(head);
 	}
 }
+
