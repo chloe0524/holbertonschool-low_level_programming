5f6ecbe88aa9469277a2a927acb48c9e014bbda6
chloe0524 <chloco31@gmail.com>
2023-12-04 04:58:55 -0800

    create file, deleted

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,25 +10,27 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file;
-	int bytes_read = 0;
+	int file, write_status;
 
-	if (filename == NULL || text_content == 0)
-		return (-1);
+	if (filename == NULL)
+		return -1;
 
 	file = open(filename, O_WRONLY | O_CREAT, 0664);
-	if (file == -1)
-	{
-		return (-1);
-	}
 
-	bytes_read = read(file, text_content, strlen(text_content));
-	if (bytes_read == -1)
+	if (file == -1)
+		return -1;
+
+	if (text_content != NULL)
 	{
-		close(file);
-		return (-1);
+		write_status = write(file, text_content, strlen(text_content));
+
+		if (write_status == -1)
+		{
+			close(file);
+			return -1;
+		}
 	}
 
 	close(file);
-	return (1);
+	return 1;
 }
