2e1ab1357f201332daee579a3f4f4378a2e1e705
chloe0524 <chloco31@gmail.com>
2023-12-04 01:26:35 -0800

    header file

---
+++ file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
