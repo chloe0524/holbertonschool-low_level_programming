147dddfe7e6e35f0821a3faf7d2e308431a0d41e
chloe0524 <chloco31@gmail.com>
2023-12-04 05:31:35 -0800

    append with wrong output

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to a file
+ * @filename: the name of the file to be created or appended to
+ * @text_content: the text to be written to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t text_size = 0;
+	ssize_t write_status;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[text_size] != '\0')
+			text_size++;
+
+		write_status = write(fd, text_content, text_size);
+
+		if (write_status == -1 || write_status != text_size)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
+
