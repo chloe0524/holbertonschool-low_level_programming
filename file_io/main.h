5304a329fa51a657828c6f31f70035ed93030896
chloe0524 <chloco31@gmail.com>
2023-12-05 02:08:06 -0800

    added function prototypes

--- file_io/main.h
+++ file_io/main.h
@@ -5,10 +5,16 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int file_init(char *file_back, char *file_go, int *fd_back, int *fd_go);
+int error_copy(int fd_from, int fd_to);
+int error_close(int fd_from, int fd_to);
 
 
 #endif
