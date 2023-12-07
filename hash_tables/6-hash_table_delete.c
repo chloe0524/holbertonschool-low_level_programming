67f6641f0974dd77e989d295563d3c519785ed65
chloe0524 <chloco31@gmail.com>
2023-12-07 05:08:25 -0800

    test ro

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,28 +1,27 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - deletes a hash table + what's inside it
- * @ht: hash table to delete
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table
  */
-
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *node, *tmp;
+	hash_node_t *current, *next;
 
 	if (ht == NULL)
 		return;
 
 	for (i = 0; i < ht->size; i++)
 	{
-		node = ht->array[i];
-		while (node)
+		current = ht->array[i];
+		while (current != NULL)
 		{
-			tmp = node->next;
-			free(node->key);
-			free(node->value);
-			free(node);
-			node = tmp;
+			next = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = next;
 		}
 	}
 
