19bdafa9508293aeade27b9137128b962547b0f9
chloe0524 <chloco31@gmail.com>
2024-01-17 07:22:08 -0800

    task 1 and 6-O

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,52 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted integer array
+ * @array: pointer to first element of array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: index where value is located, or -1 if not found
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int median = 0;
+	size_t i = 0;
+	size_t j = size - 1;
+
+	if (array)
+		while (i <= j)
+		{
+			print_array(array, i, j);
+
+			median = (i + j) / 2;
+			if (array[median] == value)
+				return (median);
+			if (array[median] < value)
+				i = median + 1;
+			else if (array[median] > value)
+				j = median - 1;
+			else
+				return (median);
+		}
+	return (-1);
+}
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(int *array, int i, int j)
+{
+	printf("Searching in array: ");
+	while (i <= j)
+	{
+		printf("%d", array[i]);
+		i++;
+		if (i <= j)
+			printf(", ");
+	}
+	printf("\n");
+}
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_array(int *array, int i, int j);
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
